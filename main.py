2911696cd113381cd7123af8d57d934f1186fef1
Choco <Choco>
2017-02-05 08:55:13 -0500

    Use list comprehensions and fix syntax warnings

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import requests
-import json
 from bs4 import BeautifulSoup
+
 
 def get_title(soup):
     return soup.select_one("h1.article-header-title").get_text()
 
-def get_ingredients(soup):
-    #TODO: Use map function here instead of explicit array.
-    ingredients = []
-    for elem in soup.select("li[itemprop=ingredients]"):
-        ingredients.append((elem.select_one(".recipe-list-quantity").get_text().strip() +
-                           " " +
-                           elem.select_one(".recipe-list-item-name").get_text().strip()).strip())
 
-    return ingredients
+def get_ingredients(soup):
+    return [(elem.select_one(".recipe-list-quantity").get_text().strip() + " " +
+            elem.select_one(".recipe-list-item-name").get_text().strip())
+            .strip() for elem in soup.select("li[itemprop=ingredients]")]
+
 
 def get_directions(soup):
-    #TODO: Use map function here instead of explicit array.
-    directions = []
-    for elem in soup.select("li[itemprop=recipeInstructions]"):
-        directions.append(elem.get_text().strip())
-    return directions
+    return [elem.get_text().strip()
+            for elem in soup.select("li[itemprop=recipeInstructions]")]
 
 
 url = 'https://food52.com/recipes/25530-joanne-chang-s-hot-and-sour-soup'
@@ -32,11 +26,8 @@ headers = {'User-Agent': user_agent}
 response = requests.get(url, headers=headers)
 if(response.status_code == 200):
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(get_title(soup))
     obj = {
-        "title" : get_title(soup),
-        "ingredients" : get_ingredients(soup),
-        "directions" : get_directions(soup)
+        "title": get_title(soup),
+        "ingredients": get_ingredients(soup),
+        "directions": get_directions(soup)
     }
-    print(obj)
-    #print(json.dumps(obj, indent=4, separators=(',', ': ')))
